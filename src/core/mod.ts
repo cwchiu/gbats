54a3df8dd0dca1c699fca83a6bf3dd6182d9aed0
Chui-Wen Chiu <sisimi.pchome@gmail.com>
2020-06-22 11:30:36 +0800

    Update mod.ts

--- src/core/mod.ts
+++ src/core/mod.ts
@@ -1,9 +1,5 @@
 import ARMCore from "./ARMCore.ts";
-import ARMCoreArm from "./ARMCoreArm.ts";
-import ARMCoreThumb from "./ARMCoreThumb.ts";
 
 export {
-    ARMCore,
-    ARMCoreArm,
-    ARMCoreThumb
+    ARMCore
 };
